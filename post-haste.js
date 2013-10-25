bddf2143267bf2708e55415cb675fb4b9100affa
Ryan Debeasi <rdebeasi@gmail.com>
2013-10-24 20:18:38 -0400

    Just a normal form so far.

---
+++ post-haste.js
@@ -0,0 +1,7 @@
+(function() {
+	var dataElem = document.getElementById('data'),
+		submitElem = document.getElementById('submit');
+	submitElem.addEventListener('click', function(ev) {
+		// TODO: Submit the form!
+	});
+})();
