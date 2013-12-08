85f395d893dbe3f47545a143f8060f11eec8f283
Ryan Debeasi <rdebeasi@gmail.com>
2013-12-08 17:59:01 -0500

    Try connecting to a local instance of haste-server using JSONP. Not working just yet.

--- post-haste.js
+++ post-haste.js
@@ -1,7 +1,25 @@
-(function() {
-	var dataElem = document.getElementById('data'),
-		submitElem = document.getElementById('submit');
-	submitElem.addEventListener('click', function(ev) {
-		// TODO: Submit the form!
+jQuery(function($){
+	var $data = $('#data'),
+		$submit = $('#submit'),
+		$mainForm = $('#mainForm'),
+		url = $mainForm.attr('action');
+
+	$submit.on('click', function(ev) {
+		// This doesn't work yet.
+		// More info: https://github.com/seejohnrun/haste-server/issues/47
+
+		ev.preventDefault();
+		console.log(url);
+		$.ajax({
+			dataType : "jsonp",
+			url : url,
+			data : "hi",
+			cache : true, // For debugging - maybe haste-server doesn't like this?
+			success: function(response) {
+				alert('success!')
+			}
+		});
 	});
-})();
+
+});
+
