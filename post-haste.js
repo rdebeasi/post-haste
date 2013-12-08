32c0f9736919a33136522e065da28140c9b99bbd
Ryan Debeasi <rdebeasi@gmail.com>
2013-12-08 18:06:14 -0500

    Submit form data instead of test data.

--- post-haste.js
+++ post-haste.js
@@ -9,14 +9,13 @@ jQuery(function($){
 		// More info: https://github.com/seejohnrun/haste-server/issues/47
 
 		ev.preventDefault();
-		console.log(url);
 		$.ajax({
 			dataType : "jsonp",
 			url : url,
-			data : "hi",
+			data : $mainForm.serialize(),
 			cache : true, // For debugging - maybe haste-server doesn't like this?
 			success: function(response) {
-				alert('success!')
+				console.log('success!')
 			}
 		});
 	});
